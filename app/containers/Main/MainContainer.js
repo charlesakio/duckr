873c26586c01f7a6c1f38da528d6c98a3c6f7ec0
Charles Javelona <charlesjavelona@gmail.com>
2016-05-11 17:12:05 -0400

    Home and Main working

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { container, innerContainer } from './styles.css'
+
 const MainContainer = React.createClass({
   render () {
     return (
-      <p>{`llo Test Hot Modul`}</p>
+      <div className={container}>
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
+      </div>
     )
   },
 })
